6bcbe49bc87a4549430dad753a4257e71bd33d95
DavyMac <davymac1@yahoo.co.uk>
2018-06-16 14:15:33 +0100

    Committing initial code

---
+++ HelloWorld/src/HelloWorld.java
@@ -0,0 +1,13 @@
+
+public class HelloWorld {
+
+	/*
+	 * Change this method so that it prints your name
+	 */
+	public static void main(String[] args) {
+
+		System.out.println("Hello World");
+
+	}
+
+}
